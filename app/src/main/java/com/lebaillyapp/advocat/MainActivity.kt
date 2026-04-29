4c13aa002770b21f0ca472b26705f30746c4adca
Samuel Lebailly <sam.meusa@gmail.com>
2026-04-29 20:44:19 +0200

    feature(main/): Clean and tweak the main to launch the last compo to test.

--- app/src/main/java/com/lebaillyapp/advocat/MainActivity.kt
+++ app/src/main/java/com/lebaillyapp/advocat/MainActivity.kt
@@ -8,10 +8,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import com.lebaillyapp.advocat.prototype.ShredderScreen
-import com.lebaillyapp.advocat.ui.screen.IntroScreen
-import com.lebaillyapp.advocat.ui.screen.ShaderizerScreenBox
-import com.lebaillyapp.advocat.ui.screen.VideoScreenBox
+import com.lebaillyapp.advocat.prototype.draggeur.Playground
+import com.lebaillyapp.advocat.prototype.draggeur.PlaygroundLocked
+import com.lebaillyapp.advocat.prototype.shredder.ShredderScreen
 import com.lebaillyapp.advocat.ui.theme.AdvocatTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,13 +21,23 @@ class MainActivity : ComponentActivity() {
             AdvocatTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
+                    //todo - Main intro - logo app + LCD + CRT shader + debug
                   //  IntroScreen(modifier = Modifier.padding(innerPadding))
 
-                    //  ShaderizerScreenBox(modifier = Modifier.padding(innerPadding))
+                    //todo - Big picture - with LCD + CRT shader + debug
+                  //  ShaderizerScreenBox(modifier = Modifier.padding(innerPadding))
 
+                    //todo - Video content - with LCD + CRT shader + debug
                    // VideoScreenBox(modifier = Modifier.padding(innerPadding))
 
-                    ShredderScreen(modifier = Modifier.padding(innerPadding))
+                    //todo - Card shredder effect prototype
+                  //  ShredderScreen(modifier = Modifier.padding(innerPadding))
+
+                    //todo - Draggeur prototype
+                 //   Playground(modifier = Modifier.padding(innerPadding))
+
+                    PlaygroundLocked(modifier = Modifier.padding(innerPadding))
+
                 }
             }
         }
